240940593a5dcb5e0e6f9598cc5bd04d54ec1709
Vishnu Dawah <vkdawah@gmail.com>
2024-04-06 17:18:56 -0400

    Update server file to use env variables

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
 // PostgreSQL connection configuration
 const pool = new Pool({
-  user: "your_database_user",
-  host: "localhost",
-  database: "your_database_name",
-  password: "your_database_password",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Test endpoint
